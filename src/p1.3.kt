c74ae0be1000e4e932816f7566795aeb93d051bd
yash318 <yashc3684@gmail.com>
2026-07-02 13:14:14 +0530

    intial commint

---
+++ src/p1.3.kt
@@ -0,0 +1,41 @@
+fun main(){
+//    var s1:String ?= "23"
+//    val a1:Int =s1!!.toInt()
+//    println(a1)
+//    println("enter number")
+//    var s2=readln().toInt()
+//    println(s2)
+    println("enter student er no: ")
+    var a1: Long=readln().toLong()
+
+    println("enter student name: ")
+    var a2: String=readln().toString()
+
+    println("enter student branch: ")
+    var a3: String=readln().toString()
+
+    println("enter student class: ")
+    var a4: String=readln().toString()
+
+    println("enter student Batch: ")
+    var a5: String=readln().toString()
+
+    println("enter student clg name: ")
+    var a6: String=readln().toString()
+
+    println("enter student university name: ")
+    var a7: String=readln().toString()
+
+    println("enter student age: ")
+    var a8: Int=readln().toInt()
+
+    println("Student data")
+    println("er no: $a1")
+    println("Name: $a2")
+    println("branch: $a3")
+    println("class: $a4")
+    println("batch: $a5")
+    println("collage Name: $a6")
+    println("University name: $a7")
+    println("Age: $a8")
+}
